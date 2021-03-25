e47a23492867ff7db00917e5d7f01aa75180c3f2
Phillips <verkeiac@yahoo.com>
2021-03-24 20:26:22 -0400

    first commit to lab 3.3 data structures

---
+++ Lab3.3DataStructuresExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Lab3._3DataStructuresExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Please enter some text you would like to reverse:");
+            string text = Console.ReadLine();
+
+            ReverseInput(text);
+           
+        }
+
+        static string ReverseInput(string text)
+        {
+            Regex validate = new Regex(@"^([a-zA-Z]||\s)+$");
+
+            Stack<char> mystack = new Stack<char>();
+            char[] letters = text.ToCharArray();
+            for (int i = 0; i < letters.Length; i++)
+            {
+        
+                mystack.Push(letters[i]);
+            }         
+                 
+            if (validate.IsMatch(text))
+            {
+                Console.Write("Your text you entered in reverse is:");
+                string newstring = "";
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+
+                    char next = mystack.Pop();
+                    // newstring = newstring + next;
+                    newstring += next;
+                    // Console.Write(next);
+                }
+
+                Console.WriteLine(newstring);
+
+                                  
+            }
+            else
+            {
+               Console.WriteLine("Please only enter words and not symbols or numbers.");
+                   
+            }                      
+            return text;
+        }
+
+        //function called split. If you want to figure out the extended exercises.
+
+       
+    }
+}
